8824ede11a77fe7f508b36ca511958c047283268
Zachary Joswick <zjoswick@gmail.com>
2019-02-28 12:14:31 -0500

    removed unnecessary spaces

--- components/CodeRx/app.py
+++ components/CodeRx/app.py
@@ -3,10 +3,8 @@ import time
 import redis
 from flask import Flask
 
-
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
-
 
 def get_hit_count():
     retries = 5
@@ -18,7 +16,6 @@ def get_hit_count():
                 raise exc
             retries -= 1
             time.sleep(0.5)
-
 
 @app.route('/')
 def hello():
