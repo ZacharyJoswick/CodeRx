c4900fe2077cc842b8b503684d3e0f30564097d4
Zachary Joswick <zjoswick@gmail.com>
2019-03-04 12:34:05 -0500

    Got initial framework in place, started using templates. Applied style

--- components/CodeRx/coderx.Dockerfile
+++ components/CodeRx/coderx.Dockerfile
@@ -1,14 +1,35 @@
 FROM python:3.7-alpine
 
-ADD . /code
+#Create code/CodeRx directory
+RUN mkdir -p /code/CodeRx
 
+#Set the working directory to /code
 WORKDIR /code
 
-ENV FLASK_APP app.py
+#Add our directory to the python path
+RUN export PYTHONPATH="${PYTHONPATH}:/code"
 
+#Copy in requirements and start 
+#This speeds up build time later as we are not modifying these files often
+COPY requirements.txt /code/
+
+#Install requirements
 RUN pip install -r requirements.txt
 
+COPY start.sh /code/start.sh
+
+#Make start script executable
 RUN chmod a+x ./start.sh
 
+#Set start command
 CMD ["sh", "start.sh"]
+
+#Set flask application environment variable
+ENV FLASK_APP app.py
+
+#add the rest of the files
+ADD . /code/CodeRx
+
+
+
 # CMD ["python", "app.py"]
--- components/CodeRx/routes.py
+++ components/CodeRx/routes.py
@@ -1,9 +1,11 @@
 import time
+import os
 
 import redis
-from flask import Flask
+from flask import url_for, send_from_directory, render_template
 
-app = Flask(__name__)
+from CodeRx import app
+
 cache = redis.Redis(host='redis', port=6379)
 
 def get_hit_count():
@@ -17,10 +19,16 @@ def get_hit_count():
             retries -= 1
             time.sleep(0.5)
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 @app.route('/')
-def hello():
+def index():
     count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    # return 'Hello World! I have been seen {} times.\n'.format(count)
+    return render_template('index.html', title='Home', count=count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
