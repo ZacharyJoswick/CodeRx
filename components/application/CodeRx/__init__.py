2044c56436ade0a792c75d7a3ef489274d4839e8
RajPatel0929 <rajpatel0929@gmail.com>
2019-03-29 17:11:28 -0400

    initial database schema setup

--- components/application/CodeRx/__init__.py
+++ components/application/CodeRx/__init__.py
@@ -7,6 +7,8 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_socketio import SocketIO
 from CodeRx.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,8 +17,7 @@ app.debug = True
 socketio= SocketIO(app, message_queue='redis://redis', async_mode='eventlet')
 
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from CodeRx import routes
-
-
-
+from CodeRx import routes, models
--- components/application/CodeRx/config.py
+++ components/application/CodeRx/config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://db/'
+        'postgresql://postgres:CodeRxYourDailyDoseOfCode@db/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     # MAIL_SERVER = os.environ.get('MAIL_SERVER')
