9865f9570e4066348d030580c4d2c5280855c507
RajPatel0929 <rajpatel0929@gmail.com>
2019-04-16 16:37:12 -0400

    update models.py
    added two tables

--- components/application/CodeRx/models.py
+++ components/application/CodeRx/models.py
@@ -3,7 +3,9 @@ from flask_security import UserMixin, RoleMixin
 
 roles_users = db.Table('roles_users',
         db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')),
+        db.Column('prob_id', db.Integer(), db.ForeignKey('prob_id')),
+        db.Column('submitted_id', db.Integer(), db.ForeignKey('submitted_id'))
 
 class Role(db.Model, RoleMixin):
     id = db.Column(db.Integer(), primary_key=True)
@@ -18,3 +20,13 @@ class User(db.Model, UserMixin):
     confirmed_at = db.Column(db.DateTime())
     roles = db.relationship('Role', secondary=roles_users,
                             backref=db.backref('users', lazy='dynamic'))
+                       
+class Problem(db.Model, ProblemMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    number = db.Column(db.Integer, unique=True)
+    description = db.Column(db.String(255))
+                       
+class Submitted(db.Model, SumbittedMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer, unique=True)
+    description = db.Column(db.String(255))
