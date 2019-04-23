c4aa75d48ce9fbcfaac846fd41554359615e8233
RajPatel0929 <rajpatel0929@gmail.com>
2019-04-23 02:49:27 -0400

    Update models.py

--- components/application/CodeRx/models.py
+++ components/application/CodeRx/models.py
@@ -18,6 +18,7 @@ class User(db.Model, UserMixin):
     confirmed_at = db.Column(db.DateTime())
     roles = db.relationship('Role', secondary=roles_users,
                             backref=db.backref('users', lazy='dynamic'))
+                            
 '''
 roles_probs = db.Table('roles_probs',
     db.Column('prob_id', db.Integer(), db.ForeignKey('prob_id')),
