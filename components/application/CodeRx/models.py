a37da019bef27396b9ed45349d1ecb116f9f2dbb
RajPatel0929 <rajpatel0929@gmail.com>
2019-04-28 14:29:18 -0400

    udpate models.py
    new_field = db.Column(db.String) //this was added by mistake
    
    just removed it

--- components/application/CodeRx/models.py
+++ components/application/CodeRx/models.py
@@ -39,8 +39,7 @@ class Problem(db.Model):
 class Submission(db.Model):
     __tablename__ = 'submission'
     id = db.Column(db.Integer, primary_key=True)
-    files = db.Column(db.String)
-    new_field = db.Column(db.String)
+    files = db.Column(db.String) 
     problem_id = db.Column(db.Integer, db.ForeignKey("problem.id"))
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     test_case_results = db.relationship("Test_case_results")
