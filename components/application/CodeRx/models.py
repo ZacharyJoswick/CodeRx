aab8592aa3546b584c4684e37759a9204e00529a
Zachary Joswick <zjoswick@gmail.com>
2019-04-28 11:52:51 -0400

    Manually merged in changes from Raj branch

--- components/application/CodeRx/models.py
+++ components/application/CodeRx/models.py
@@ -1,54 +1,67 @@
 from CodeRx import db
 from flask_security import UserMixin, RoleMixin
+from datetime import datetime
 
 roles_users = db.Table('roles_users',
-        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+        db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer, db.ForeignKey('role.id')))
 
 class Role(db.Model, RoleMixin):
-    id = db.Column(db.Integer(), primary_key=True)
+    __tablename__ = 'role'
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
-    active = db.Column(db.Boolean())
-    confirmed_at = db.Column(db.DateTime())
+    active = db.Column(db.Boolean)
+    confirmed_at = db.Column(db.DateTime)
     roles = db.relationship('Role', secondary=roles_users,
                             backref=db.backref('users', lazy='dynamic'))
-        
-roles_probs = db.Table('roles_probs',
-    db.Column('prob_id', db.Integer(), db.ForeignKey('problem.id')),
-    db.Column('submitted_id', db.Integer(), db.ForeignKey('submitted.id')))
+    submissions = db.relationship("Submission")
 
 class Problem(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    description = db.Column(db.String(255))
-    language = db.Column(db.String(225))
-    testcase = db.Column(db.String(225))
-    initial_file = db.Column(db.String(225))
-    allowmultiplefiles = db.Column(db.String(225))
-    created_date = db.Column(db.DateTime())
-    due_date = db.Column(db.DateTime())
-class Submitted(db.Model):
+    __tablename__ = 'problem'
     id = db.Column(db.Integer, primary_key=True)
-    prob_id = db.Column(db.Integer, unique=True)
-    user_id = db.Column(db.Integer, unique=True)
-    files = db.Column(db.String(225))
-    new_field = db.Column(db.String(225))
-    test_case_results = db.Column(db.String(255))
-class files(db.Model):
+    description = db.Column(db.String)
+    language = db.Column(db.String)
+    allowmultiplefiles = db.Column(db.Boolean)
+    entry_command = db.Column(db.String)
+    created_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    due_date = db.Column(db.DateTime)
+    submissions = db.relationship("Submission")
+    test_cases = db.relationship("TestCase")
+    files = db.relationship("Files")
+
+class Submission(db.Model):
+    __tablename__ = 'submission'
     id = db.Column(db.Integer, primary_key=True)
-    file_name = db.Column(db.String(225))
-    content = db.Column(db.String(225))
-class testcase(db.Model):
+    files = db.Column(db.String)
+    new_field = db.Column(db.String)
+    problem_id = db.Column(db.Integer, db.ForeignKey("problem.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    test_case_results = db.relationship("Test_case_results")
+
+class Files(db.Model):
+    __tablename__ = 'files'
     id = db.Column(db.Integer, primary_key=True)
-    input = db.Column(db.String(225))
-    expected_output = db.Column(db.String(225))
-    hidden = db.Column(db.String(225))
-class test_case_results(db.Model):
+    file_name = db.Column(db.String)
+    content = db.Column(db.String)
+    problem_id = db.Column(db.Integer, db.ForeignKey("problem.id"))
+
+class TestCase(db.Model):
+    __tablename__ = 'testcase'
     id = db.Column(db.Integer, primary_key=True)
-    testcase_id = db.Column(db.String(225))
-    passs = db.Column(db.String(225))
+    input = db.Column(db.String)
+    expected_output = db.Column(db.String)
+    hidden = db.Column(db.Boolean)
+    problem_id = db.Column(db.Integer, db.ForeignKey("problem.id"))
+
+class Test_case_results(db.Model):
+    __tablename__ = 'test_case_results'
+    id = db.Column(db.Integer, primary_key=True)
+    pass_state = db.Column(db.Boolean)
+    testcase_id = db.Column(db.Integer, db.ForeignKey("submission.id"))
